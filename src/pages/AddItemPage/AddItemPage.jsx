97a0340b3e76941f981139fd161eeed05002e5b2
Kathleen Kang <kathleen@treasurer.co.kr>
2024-04-22 22:26:22 +0900

    feat: add styled-components to /additem page

--- src/pages/AddItemPage/AddItemPage.jsx
+++ src/pages/AddItemPage/AddItemPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import {
+  Button,
+  Container,
+  FlexContainer,
+  SectionTitle,
+} from "../../styles/CommonStyles";
 
 function AddItemPage() {
-  return <div>AddItemPage</div>;
+  return (
+    <Container>
+      <FlexContainer>
+        <SectionTitle>상품 등록하기</SectionTitle>
+        <Button>등록</Button>
+      </FlexContainer>
+    </Container>
+  );
 }
 
 export default AddItemPage;
